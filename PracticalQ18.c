f127893bc0559062db12f2d968b4fa30a3c0b6d6
Esheshwari <esheshwarikumarbca24@rvu.edu.in>
2024-11-16 01:52:24 +0530

    Update PracticalQ18.c

--- PracticalQ18.c
+++ PracticalQ18.c
@@ -1,1 +1,21 @@
-
+//Program to find Sum of Digits
+#include<stdio.h>
+int SumofDigits(int num)
+{
+    int sum=0;
+    while(num!=0)
+    {
+        sum+=num%10;
+        num/=10;
+    }
+    return sum;
+}
+int main()
+{
+    int num;
+    printf("enter a number:");
+    scanf("%d", &num);
+    int results= SumofDigits( num);
+    printf("the sum of the digit of %d is: %d", num, results);
+    return 0;
+}
