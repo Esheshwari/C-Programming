3688e5c0d6f5be3dd747ff4c27ad164a090945a8
Esheshwari <esheshwarikumarbca24@rvu.edu.in>
2024-11-16 02:14:15 +0530

    Create PracticalQ36.c

---
+++ PracticalQ36.c
@@ -0,0 +1,32 @@
+//prime or composite using recurssion
+#include<stdio.h>
+int isPrime(n, i)
+{
+    if(n<=2)
+    {
+        return(n==2)?1:0;
+    }
+    if(n%i==0)
+    {
+        return 0;
+    }
+    if(i*i>n)
+    {
+        return 1;
+    }
+    return isPrime(n, i+1);
+}
+int main()
+{
+    int n;
+    printf("enter the number: ");
+    scanf("%d", &n);
+    if(isPrime(n,2))
+    {
+        printf("%d is a prime number. \n", n);
+    }
+    else{
+        printf("%d is a composite number: \n", n);
+    }
+    return 0;
+}
